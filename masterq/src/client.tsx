30c6e63a616935fde26758d4b2bcc0149bb1c348
ntk221 <nittakdub@gmail.com>
2024-06-10 23:35:36 +0900

    fix: HashRouterに修正

--- masterq/src/client.tsx
+++ masterq/src/client.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import App from "./app";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
+  if (!window.location.hash) {
+    window.location.hash = "#/";
+  }
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/:filename" element={<App />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>
   );
 }
--- masterq/src/index.tsx
+++ masterq/src/index.tsx
@@ -4,32 +4,32 @@ import { renderToString } from "react-dom/server";
 const app = new Hono();
 
 app.get("*", (c) => {
-  return c.html(
-    renderToString(
-      <html>
-        <head>
-          <meta charSet="utf-8" />
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
-          <link
-            rel="stylesheet"
-            href="https://cdn.simplecss.org/simple.min.css"
-          />
-          {import.meta.env.PROD ? (
-            <>
-              <script type="module" src="/static/client.js"></script>
-            </>
-          ) : (
-            <>
-              <script type="module" src="/src/client.tsx"></script>
-            </>
-          )}
-        </head>
-        <body>
-          <div id="root"></div>
-        </body>
-      </html>
-    )
+  const html = renderToString(
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.simplecss.org/simple.min.css"
+        />
+        {import.meta.env.PROD ? (
+          <>
+            <script type="module" src="/static/client.js"></script>
+          </>
+        ) : (
+          <>
+            <script type="module" src="/src/client.tsx"></script>
+          </>
+        )}
+      </head>
+      <body>
+        <div id="root"></div>
+      </body>
+    </html>
   );
+
+  return c.html(html);
 });
 
 export default app;
